4de5313dbed90191d63d2782a20bfae8a550c988
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2025-03-31 21:45:04 -0400

    basic layout and puzzle progression complete
    Just missing art assets and more background items to interact with

--- Project_RATS/Game/RATS_Build/Assets/Scripts/DialogueLineTimeArray.cs
+++ Project_RATS/Game/RATS_Build/Assets/Scripts/DialogueLineTimeArray.cs
@@ -7,9 +7,21 @@ using UnityEngine.UIElements;
 [SerializableAttribute]
 public class DialogueLineTimeArray : MonoBehaviour //ScriptableObject 
 {
+    [SerializeField] bool repeatLines;
     public string NPCName;
     public List<string> DayEarlyLines;
     public List<string> DayLateLines;
     public List<string> NightEarlyLines;
     public List<string> NightLateLines;
+
+    private void Start()
+    {
+        if (repeatLines)
+        {
+            DayLateLines = DayEarlyLines;
+            NightEarlyLines = DayEarlyLines;
+            NightLateLines = DayEarlyLines;
+        }
+    }
+
 }
